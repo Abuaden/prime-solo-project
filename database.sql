2766069b7ef77a38418ef22c73749572f6e368b1
Abubakar Aden <abubakar.aden978@gmail.com>
2023-12-28 21:44:25 -0600

    Created database tables

--- database.sql
+++ database.sql
@@ -4,7 +4,42 @@
 -- ex. SELECT * FROM "user";
 -- Otherwise you will have errors!
 CREATE TABLE "user" (
-    "id" SERIAL PRIMARY KEY,
-    "username" VARCHAR (80) UNIQUE NOT NULL,
-    "password" VARCHAR (1000) NOT NULL
+	"id" serial NOT NULL,
+	"username" varchar(256) NOT NULL UNIQUE,
+	"email" varchar(256) NOT NULL UNIQUE,
+	"password" varchar(256) NOT NULL,
+	"firstname" varchar(256) NOT NULL,
+	"lastname" varchar(256) NOT NULL,
+	"current_level" integer NOT NULL,
+	"role" varchar(256) NOT NULL DEFAULT 'user',
+	"flashcards_flipped" TEXT [],
+	CONSTRAINT "user_pk" PRIMARY KEY ("id")
 );
+
+
+
+CREATE TABLE "flashcards" (
+	"id" serial NOT NULL,
+	"arabicword" varchar(256) NOT NULL,
+	"englishword" varchar(256) NOT NULL,
+	"level_id" integer NOT NULL,
+	CONSTRAINT "flashcards_pk" PRIMARY KEY ("id")
+);
+
+
+
+CREATE TABLE "levels" (
+	"id" serial NOT NULL,
+	"name" varchar(256) NOT NULL,
+	CONSTRAINT "levels_pk" PRIMARY KEY ("id")
+);
+
+
+ALTER TABLE "user" ADD CONSTRAINT "user_fk0" FOREIGN KEY ("current_level") REFERENCES "levels"("id");
+
+ALTER TABLE "flashcards" ADD CONSTRAINT "flashcards_fk0" FOREIGN KEY ("level_id") REFERENCES "levels"("id");
+
+
+
+
+
