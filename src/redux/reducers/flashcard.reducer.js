fe2bd995af1f692b462f71fd1aa0c3179da8d356
Abubakar Aden <abubakar.aden978@gmail.com>
2024-01-10 22:41:50 -0600

    create flashcard reducer

---
+++ src/redux/reducers/flashcard.reducer.js
@@ -0,0 +1,16 @@
+const flashcardReducer = (state = [], action) => {
+  switch (action.type) {
+    case 'SET_FLASHCARDS':
+      return action.payload;
+    case 'UNSET_USER':
+      return {};
+    default:
+      return state;
+  }
+};
+
+// user will be on the redux state at:
+// state.user
+export default flashcardReducer;
+flashcardReducer.js
+
